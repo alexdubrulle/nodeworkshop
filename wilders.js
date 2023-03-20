ef3a89500c5cf4d3ff826877833fef67183ebf37
Alex DUBRULLE--FAGNONI <alexdubrullefagnoni26@hotmail.fr>
2023-03-20 10:57:41 +0100

    workshop1

---
+++ wilders.js
@@ -0,0 +1,5 @@
+import chalk from "chalk"
+console.log(chalk.bgCyanBright.yellowBright("Hello Wilhem !"));
+console.log(chalk.bgRedBright.magenta("Hello Nicolas !"));
+console.log(chalk.bgMagentaBright.green("Hello Thomas !"));
+console.log(chalk.bgWhiteBright.redBright("Hello Julien !"));
